a21afb7e1b522e767b43fa16d05556ea3feddb2d
Ka-Chun Cheung <kachun7@gmail.com>
2020-03-04 17:21:24 +0100

    Move API test to another path

--- test/data/remote_datasource/api_test.dart
+++ test/data/remote_datasource/api_test.dart
@@ -1,15 +1,15 @@
-import 'package:code_test/models/application.dart';
-import 'package:code_test/api/api.dart';
+import 'package:code_test/data/remote_datasource/api.dart';
+import 'package:code_test/domain/models/application.dart';
 import 'package:test/test.dart';
 
-main() {
-  group("Submit Application", () {
-    final testApplication = Application(firstName: "test", lastName: "testsson", email: "194907011813", loanAmount: 500000);
+void main() {
+  group('Submit Application', () {
+    final testApplication = Application(firstName: 'test', lastName: 'testsson', email: '194907011813', loanAmount: 500000);
 
-    test("Submit should return successfullly with the application", () async {
+    test('Submit should return successfullly with the application', () async {
       // Prepare
-      final errorChance = null; // Should not simulate an error
-      final delay = 4;
+      const int errorChance = null; // Should not simulate an error
+      const delay = 4;
       final repo = MockAPI.test(errorChance: errorChance, delay: delay);
 
       // Test
@@ -20,19 +20,19 @@ main() {
       expect(resp, testApplication);
     });
 
-    test("Submit should throw an [ArgumentError] when submit is missing the argument application", () async {
+    test('Submit should throw an [ArgumentError] when submit is missing the argument application', () async {
       // Prepare
-      final errorChance = null; // Should not simulate an error
-      final delay = 4;
+      const int errorChance = null; // Should not simulate an error
+      const delay = 4;
       final repo = MockAPI.test(errorChance: errorChance, delay: delay);
       // Test & assert that an ArgumentError was thrown
       expect(repo.submitApplication(null), throwsArgumentError);
     });
 
-    test("Submit should throw an [Exception] when submit fails to persist the application", () async {
+    test('Submit should throw an [Exception] when submit fails to persist the application', () async {
       // Prepare
-      final errorChance = 1; // 1 in 1 chance of throwing an error
-      final delay = 4;
+      const int errorChance = 1; // 1 in 1 chance of throwing an error
+      const delay = 4;
       final repo = MockAPI.test(errorChance: errorChance, delay: delay);
 
       // Test & assert that an ApiTimeoutException was thrown
@@ -40,11 +40,11 @@ main() {
     });
   });
 
-  group("Get Applications", () {
-    test("Get should return successfullly with the applications", () async {
+  group('Get Applications', () {
+    test('Get should return successfullly with the applications', () async {
       // Prepare
-      final errorChance = null; // Should not simulate an error
-      final delay = 4;
+      const int errorChance = null; // Should not simulate an error
+      const delay = 4;
       final repo = MockAPI.test(errorChance: errorChance, delay: delay);
 
       // Test
@@ -55,10 +55,10 @@ main() {
       expect(resp.length, 8);
     });
 
-    test("Should throw an [Exception] when get fails to retrieve the applications", () async {
+    test('Should throw an [Exception] when get fails to retrieve the applications', () async {
       // Prepare
-      final errorChance = 1; // 1 in 1 chance of throwing an error
-      final delay = 4;
+      const errorChance = 1; // 1 in 1 chance of throwing an error
+      const delay = 4;
       final repo = MockAPI.test(errorChance: errorChance, delay: delay);
 
       // Test & assert that an ApiTimeoutException was thrown
