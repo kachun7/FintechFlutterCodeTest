a00d63c1cab474beb1e8b4798a9ecb2899fa768e
Ka-Chun Cheung <kachun7@gmail.com>
2020-03-04 17:04:10 +0100

    Add style constants

---
+++ lib/core/theme_style.dart
@@ -0,0 +1,9 @@
+const double kMargin4 = 4.0;
+const double kMargin8 = 8.0;
+const double kMargin12 = 12.0;
+const double kMargin16 = 16.0;
+const double kMargin20 = 20.0;
+
+const double kButtonHeight = 50.0;
+
+const double kLoadingHeight = 100.0;
